2be29b8b21584d1b40ea4b0792c85564395e1d13
phj1027 <qkrgywjd981027@naver.com>
2021-08-20 02:53:05 +0900

    총 구현 시 애니메이션 미작동 수정
    Animator에 해당 Controller미적용 문제

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -2,104 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GunController : MonoBehaviour
 {
-
     [SerializeField]
     private Gun currentGun;
 
     private float currentFireRate;
 
-    private bool isReload = false;
-    public bool isFineSightMode = false;
-
-    // 본래 포지션 값.
-    [SerializeField]
-    private Vector3 originPos;
 
     private AudioSource audioSource;
 
-    void Start()
+    private void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         GunFireRateCalc();
         TryFire();
-        TryReload();
-        TryFineSight();
     }
 
     private void GunFireRateCalc()
     {
-        if (currentFireRate > 0)
-            currentFireRate -= Time.deltaTime;
-    }
+        if(currentFireRate > 0)
+        {
+            currentFireRate -= Time.deltaTime; // 60분의 1씩 60번 감소  ==  1초에 1감소시킴
+        }
 
+    }
     private void TryFire()
     {
-        if (Input.GetButton("Fire1") && currentFireRate <= 0 && !isReload)
+        if(Input.GetButton("Fire1") &&  currentFireRate <= 0)
         {
             Fire();
         }
     }
-
     private void Fire()
     {
-        if (!isReload)
+        if (currentGun.currentBulletCount > 0)
         {
-            if (currentGun.currentBulletCount > 0)
-                Shoot();
-            else
-            {
-                CancelFineSight();
-                StartCoroutine(ReloadCoroutine());
-            }
-
-
+            Shoot();
         }
+        else
+        {
+            Reload();
+        }
+       
+
     }
 
     private void Shoot()
     {
         currentGun.currentBulletCount--;
-        currentFireRate = currentGun.fireRate; // 연사 속도 재계산.
+        currentFireRate = currentGun.fireRate; // 연사 속도 재계산
         PlaySE(currentGun.fire_Sound);
         currentGun.muzzleFlash.Play();
-        StopAllCoroutines();
-        StartCoroutine(RetroActionCoroutine());
+        Debug.Log("총알발사함");
 
-        Debug.Log("총알 발사함");
 
     }
 
-    private void TryReload()
+    private void Reload()
     {
-        if (Input.GetKeyDown(KeyCode.R) && !isReload && currentGun.currentBulletCount < currentGun.reloadBulletCount)
+        if(currentGun.carryBulletCount>0)
         {
-            CancelFineSight();
-            StartCoroutine(ReloadCoroutine());
-        }
-    }
-
-    IEnumerator ReloadCoroutine()
-    {
-        if (currentGun.carryBulletCount > 0)
-        {
-            isReload = true;
-
             currentGun.anim.SetTrigger("Reload");
 
-
-            currentGun.carryBulletCount += currentGun.currentBulletCount;
-            currentGun.currentBulletCount = 0;
-
-            yield return new WaitForSeconds(currentGun.reloadTime);
-
-            if (currentGun.carryBulletCount >= currentGun.reloadBulletCount)
+            if(currentGun.carryBulletCount>=currentGun.reloadBulletCount)
             {
                 currentGun.currentBulletCount = currentGun.reloadBulletCount;
                 currentGun.carryBulletCount -= currentGun.reloadBulletCount;
@@ -108,108 +79,6 @@ public class GunController : MonoBehaviour
             {
                 currentGun.currentBulletCount = currentGun.carryBulletCount;
                 currentGun.carryBulletCount = 0;
-            }
-
-
-            isReload = false;
-        }
-        else
-        {
-            Debug.Log("소유한 총알이 없습니다.");
-        }
-    }
-
-    private void TryFineSight()
-    {
-        if (Input.GetButtonDown("Fire2") && !isReload)
-        {
-            FineSight();
-        }
-    }
-
-    public void CancelFineSight()
-    {
-        if (isFineSightMode)
-            FineSight();
-    }
-
-    private void FineSight()
-    {
-        isFineSightMode = !isFineSightMode;
-        currentGun.anim.SetBool("FineSightMode", isFineSightMode);
-
-        if (isFineSightMode)
-        {
-            StopAllCoroutines();
-            StartCoroutine(FineSightActivateCoroutine());
-        }
-        else
-        {
-            StopAllCoroutines();
-            StartCoroutine(FineSightDeactivateCoroutine());
-        }
-
-    }
-
-
-    IEnumerator FineSightActivateCoroutine()
-    {
-        while (currentGun.transform.localPosition != currentGun.fineSightOriginPos)
-        {
-            currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, currentGun.fineSightOriginPos, 0.2f);
-            yield return null;
-        }
-    }
-
-    IEnumerator FineSightDeactivateCoroutine()
-    {
-        while (currentGun.transform.localPosition != originPos)
-        {
-            currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, originPos, 0.2f);
-            yield return null;
-        }
-    }
-
-    IEnumerator RetroActionCoroutine()
-    {
-        Vector3 recoilBack = new Vector3(currentGun.retroActionForce, originPos.y, originPos.z);
-        Vector3 retroActionRecoilBack = new Vector3(currentGun.retroActionFineSightForce, currentGun.fineSightOriginPos.y, currentGun.fineSightOriginPos.z);
-
-        if (!isFineSightMode)
-        {
-
-            currentGun.transform.localPosition = originPos;
-
-            // 반동 시작
-            while (currentGun.transform.localPosition.x <= currentGun.retroActionForce - 0.02f)
-            {
-                currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, recoilBack, 0.4f);
-                yield return null;
-            }
-
-            // 원위치
-            while (currentGun.transform.localPosition != originPos)
-            {
-                currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, originPos, 0.1f);
-                yield return null;
-            }
-        }
-        else
-        {
-            currentGun.transform.localPosition = currentGun.fineSightOriginPos;
-
-            // 반동 시작
-            while (currentGun.transform.localPosition.x <= currentGun.retroActionFineSightForce - 0.02f)
-            {
-                currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, retroActionRecoilBack, 0.4f);
-                yield return null;
-            }
-
-            // 원위치
-            while (currentGun.transform.localPosition != currentGun.fineSightOriginPos)
-            {
-                currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, currentGun.fineSightOriginPos, 0.1f);
-                yield return null;
             }
         }
 
